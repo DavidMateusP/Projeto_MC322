346e5588a8991ddc8cfea9aa4709e015b3328e67
Taina <t253350@dac.unicamp.br>
2024-05-06 22:14:12 -0300

    Alteracao no esboco das classes Loan e Client

--- Client.java
+++ Client.java
@@ -2,5 +2,26 @@ import java.util.ArrayList;
 
 public class Client {
     private double balance;
-    private ArrayList<Loan> emprestimos;
+    private ArrayList<Loan> loans;
+
+    // Constructor method
+    public Client(double balance) {
+        this.balance = balance;
+        this.loans = null;
+    }
+
+    // Getters and Setters
+    public double getBalance() {
+        return balance;
+    }
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+    public ArrayList<Loan> getLoans() {
+        return loans;
+    }
+    public void setLoans(ArrayList<Loan> loans) {
+        this.loans = loans;
+    }
+    
 }
--- Loan.java
+++ Loan.java
@@ -1,16 +1,64 @@
-import java.util.Date;
+
+import java.time.LocalDate;
 
 public class Loan {
     private Client client;
     private Item item;
-    private Date deadline;
+    private LocalDate deadline;
     private double totalValue;
 
-    public boolean renew(int dias) {
+    // Getters and Setters
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDate deadline) {
+        this.deadline = deadline;
+    }
+
+    public double getTotalValue() {
+        return totalValue;
+    }
+
+    public void setTotalValue(double totalValue) {
+        this.totalValue = totalValue;
+    }
+    
+
+    // Object methods
+    public boolean renewItem(int days) {
+        // Verifies if the loan is late on the return deadline
+        if(deadline.isBefore(LocalDate.now())) {
+            System.err.println("The item borrowed is late, it could not be renewed.");
+            return false;
+        } 
+        // Adds x days to the new deadline
+        setDeadline(deadline.plusDays(days));
+        return true;
+    }
+
+    public boolean returnItem(Item item) {
         return false;
     }
 
-    public boolean giveBack() {
-        return false;
+    public int rateClient() {
+        int rating = 0;
+        return rating;
     }
 }
