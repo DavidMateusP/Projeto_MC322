5cfd06f3bb08aac5cf84fac634947d31842edf29
Taina Bastos de Almeida <ra253350@venezuela.lab.ic.unicamp.br>
2024-05-08 11:24:33 -0300

    changes on the structure of the class Loan
    Adds calcPenalty: calculates de amount of money to be payed based on the delay on the return of the item and it's original cost. Adds isLate function: verifies if the item is late or not, if it is it deducts the penalty from the clients balance. Adds constructor method. Still a scratch, methods not complete nor checked.

--- Loan.java
+++ Loan.java
@@ -1,11 +1,20 @@
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 public class Loan {
     private Client client;
     private Item item;
     private LocalDate deadline;
     private double totalValue;
+
+    // Constructor
+    public Loan(Client client, Item item, LocalDate deadline, double totalValue) {
+        this.client = client;
+        this.item = item;
+        this.deadline = deadline;
+        this.totalValue = totalValue;
+    }
 
     // Getters and Setters
     public Client getClient() {
@@ -42,9 +51,37 @@ public class Loan {
     
 
     // Object methods
+    private double calcPenalty() {
+        double penalty;
+        // penalty of 2% on the original cost of the item
+        final double PENALTYPERCENTAGE = 0.02;
+
+        // Calculates how many days late the item is
+        LocalDate currentDate = LocalDate.now();
+        double lateDays = ChronoUnit.DAYS.between(deadline, currentDate);
+
+        // Returns the penalty based on how many days are late and the original item's value
+        return (item.getCost() * PENALTYPERCENTAGE) * lateDays;
+    }
+
+    /*
+     * Verifies if the item is late 
+     * if it is late: deducts the penalty cost from the clients balance
+     * if it is not: returns false
+     */
+    public boolean isLate() {
+        if(deadline.isBefore(LocalDate.now())) {
+            double penalty = calcPenalty();
+            client.setBalance(client.getBalance() - penalty);
+            return true;
+        }
+        return false;
+    }
+
+    /*To consider: do we want a maximmum amount of days to renew? */
     public boolean renewItem(int days) {
         // Verifies if the loan is late on the return deadline
-        if(deadline.isBefore(LocalDate.now())) {
+        if(isLate()) {
             System.err.println("The item borrowed is late, it could not be renewed.");
             return false;
         } 
@@ -53,10 +90,18 @@ public class Loan {
         return true;
     }
 
-    public boolean returnItem(Item item) {
-        return false;
+    public boolean returnItem() {
+        // Verifies if the loan is late on the return deadline
+        if(isLate()) {
+            // Change the clients balance
+            System.err.println("The item is late");
+            return false;
+        } 
+        // The item is not late -> implement
+        return true;
     }
 
+    /* To implement */
     public int rateClient() {
         int rating = 0;
         return rating;
